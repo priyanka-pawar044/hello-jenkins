fdcc92e32111af53b9efc6644e8acedb45552c76
priyanka-pawar044 <pawarpriyanka6673@gmail.com>
2025-08-08 19:01:27 +0530

    Add files via upload

---
+++ display-motd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# display-motd.sh
+# A simple script to print a MOTD message
+
+MOTD_MESSAGE="hello from unnati"
+
+echo "=============================="
+echo "🚀  Message of the Day"
+echo "💬  $MOTD_MESSAGE"
+echo "=============================="
